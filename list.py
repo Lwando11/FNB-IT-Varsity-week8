7bcec3766fc968dcebac0af126fd0f0a1325a69b
Lwando Bhixa <134373350+Lwando11@users.noreply.github.com>
2025-07-16 16:44:28 +0200

    Create list.py

---
+++ list.py
@@ -0,0 +1,26 @@
+
+
+fruits = ["Apple","Pear", "Lemon", "Pear", "Grape"]
+
+print(fruits)
+
+fruits[2] = "Gauva"
+
+print(fruits)
+
+fruits.append("kiwi")
+print(fruits)
+ 
+fruits.insert(2, 'oranges')
+print(fruits)
+
+fruits.remove("Pear")
+print(fruits)
+
+numbers = [4, 56, 8, 13, 22]
+
+numbers.sort()
+print(numbers)
+print()
+numbers.sort(reverse=True)
+print(numbers)
